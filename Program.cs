55011e54b0c608e271e3d7f337ad498556d7fda1
Leonardo buta <teste@teste.com>
2024-01-17 22:14:44 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 1425)
+            {
+
+                Console.WriteLine("Senha incorreta:");
+                senha = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Ascesso Permitido!!!");
+
+        }
+    }
+}
